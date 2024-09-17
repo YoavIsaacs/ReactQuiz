7322ef1100dce408ca47fbd144ba2e3b271942ad
Yoav Isaacs <yoav.isaacs@gmail.com>
2024-09-18 00:09:35 +0300

    [Quiz] Added initial timer, needs fixing

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import QUESTIONS from '../../questions.js'
 import quizCompleteImg from '../../assets/quiz-complete.png'
+import QuestionTimer from "../QuestionTimer/QuestionTimer.jsx";
 
 export default function Quiz() {
 
@@ -34,6 +35,7 @@ export default function Quiz() {
     return (
         <div id="quiz">
             <div id="question">
+                <QuestionTimer timeout={30000} onTimeout={() => handleSelectAnswer(null)} />
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
                     {shuffledAnswers.map((answer) => (
