9ec2fa9d9308c6f634b1bcf054549c89f3476743
Yoav Isaacs <yoav.isaacs@gmail.com>
2024-09-23 23:18:45 +0300

    [Quiz] Added timer to show correct/wrong answers

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -6,16 +6,27 @@ import QuestionTimer from "../QuestionTimer/QuestionTimer.jsx";
 export default function Quiz() {
 
     const [userAnswers, setUserAnswers] = useState([]);
+    const [answerState, setAnsweredState]  = useState("");
 
-    const activeQuestionIndex = userAnswers.length;
+    const activeQuestionIndex = answerState === "" ? userAnswers.length : userAnswers.length - 1;
     const finishedAllQuestions = activeQuestionIndex === QUESTIONS.length;
 
     const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
+        setAnsweredState('answered');
         setUserAnswers((prevAnswers) => {
             const newAnswers = [...prevAnswers, selectedAnswer]
             return newAnswers;
         });
-    }, [])
+
+        setTimeout(() => {
+            if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+                setAnsweredState("correct");
+            } else {
+                setAnsweredState("wrong");
+            }
+        }, 1000)
+
+    }, [activeQuestionIndex])
 
     const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
